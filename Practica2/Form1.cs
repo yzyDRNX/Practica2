75223c62d07223bb60d9c17038730ad170706b37
yzyDRNX <edaniellriv@gmail.com>
2023-10-02 19:28:42 -0600

    Agregar archivos de proyecto.

---
+++ Practica2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float C = float.Parse(textBox2.Text);
+            float F = (C * 9f / 5f) + 32;
+            textBox1.Text = F.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0.0";
+            textBox2.Text = "0.0";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float F = float.Parse(textBox1.Text);
+            float C = (F - 32) * 5.0F / 9.0F;
+            textBox2.Text = C.ToString();
+        }
+    }
+}
